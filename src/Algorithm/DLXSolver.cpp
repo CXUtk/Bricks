11a2216717763581e7befc6b16e988b74dc7106a
CXUtk <2237367067@qq.com>
2021-01-04 16:31:24 -0500

    DLX算法爆搜精准匹配就绪

---
+++ src/Algorithm/DLXSolver.cpp
@@ -0,0 +1,18 @@
+﻿#include "DLXSolver.h"
+
+DLXSolver::DLXSolver(int r, int c) :rows(r), cols(c) {
+    memset(rowLink, 0, sizeof(RowLink) * (r + 1));
+    memset(nodes, 0, sizeof(DLXNode) * (r * c + 5));
+}
+
+void DLXSolver::link(int r, int c) {
+}
+
+void DLXSolver::remove(int c) {
+}
+
+void DLXSolver::recover(int c) {
+}
+
+void DLXSolver::solve() {
+}
---
+++ src/Algorithm/DLXSolver.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+#include <algorithm>
+#include <memory>
+struct DLXSolver {
+public:
+    DLXSolver(int r, int c);
+
+    void link(int r, int c);
+    void remove(int c);
+    void recover(int c);
+    void solve();
+
+private:
+    static constexpr int MAX_ROW = 100 * 8 * 100 * 2;
+    static constexpr int MAX_COL = 100;
+    struct DLXNode {
+        int L, R, U, D;
+        int row, col;
+    };
+    struct RowLink {
+        int head, sz;
+    };
+    int rows, cols, tot;
+    DLXNode nodes[MAX_ROW * MAX_COL];
+    RowLink rowLink[MAX_ROW];
+    int ans[MAX_ROW];
+
+
+    int newNode(int r, int c) {
+        ++tot;
+        nodes[tot].row = r, nodes[tot].col = c;
+        return tot;
+    }
+
+};
